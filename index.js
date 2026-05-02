d5bbcf39efc4b01b75be9ee97a2e09787f22c9c3
Filipe Samuel <filipesamuel214@gmail.com>
2026-05-02 01:36:32 -0300

    fix: firebase via env

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 // ─────────────────────────────────────────
 // Firebase Admin Init
 // ─────────────────────────────────────────
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
